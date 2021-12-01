3c6483bf00e20534f0a8437cc3cf7f1ba3184eca
Yewon <askges20@kyonggi.ac.kr>
2021-12-02 02:35:02 +0900

    Create 직사각형 별찍기.java

---
+++ Level1/직사각형 별찍기.java
@@ -0,0 +1,37 @@
+//StringBuilder 이용, append로 문자 이어붙여서 toString
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
+
+
+/*
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
