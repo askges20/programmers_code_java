bae76c0d53502f1880f5d17f1b4ab94adafb26bb
Yewon <askges20@kyonggi.ac.kr>
2021-12-11 12:15:04 +0900

    Create 행렬의 덧셈.java

---
+++ Level1/행렬의 덧셈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
+
+// 배열 길이 : length
+// 콜렉션 길이 : size()
+// 문자열 길이 : length()
